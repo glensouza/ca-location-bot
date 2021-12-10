1c222d1d3025671f74c5da527ea23aaa53dfc8d8
Glen Souza <glenster75@gmail.com>
2021-12-09 19:12:06 -0800

    fix build

--- Api/WeatherForecastFunction.cs
+++ Api/WeatherForecastFunction.cs
@@ -6,46 +6,29 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
 
-using BlazorApp.Shared;
-
 namespace BlazorApp.Api
 {
     public static class WeatherForecastFunction
     {
         private static string GetSummary(int temp)
         {
-            var summary = "Mild";
-
-            if (temp >= 32)
+            string summary = temp switch
             {
-                summary = "Hot";
-            }
-            else if (temp <= 16 && temp > 0)
-            {
-                summary = "Cold";
-            }
-            else if (temp <= 0)
-            {
-                summary = "Freezing!";
-            }
+                >= 32 => "Hot",
+                <= 16 and > 0 => "Cold",
+                <= 0 => "Freezing!",
+                _ => "Mild"
+            };
 
             return summary;
         }
 
         [FunctionName("WeatherForecast")]
-        public static IActionResult Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
-            ILogger log)
+        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req, ILogger log)
         {
-            var randomNumber = new Random();
-            var temp = 0;
+            Random randomNumber = new();
 
-            var result = Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = temp = randomNumber.Next(-20, 55),
-                Summary = GetSummary(temp)
-            }).ToArray();
+            int[] result = Enumerable.Range(1, 5).ToArray();
 
             return new OkObjectResult(result);
         }
